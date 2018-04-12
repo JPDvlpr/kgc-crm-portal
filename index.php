e594db3acf921718996645f95e5e479bb7726a46
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-04-12 17:35:35 -0400

    basic index, header, and footer - still refer to old project

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    require_once("views/standard_peices.php");
+    standard_header("sample","");
+
+    echo "hello 3";
+
+    standard_footer();
+?>
---
+++ views/standard_peices.php
@@ -0,0 +1,88 @@
+<?php
+function standard_header($title, $stylesheet ){
+?>
+<!--
+    header.html
+    IT-328
+    Cat-Wishes Final Project
+    This file has the head and header (nav bar) for Cat-Wishes web site.
+    Bessy Torres-Miller
+    Melanie Felton
+
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php $title ?></title>
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <!--JQuery link-->
+    <script src="http://code.jquery.com/jquery.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <link href="/styles/home.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+
+
+<!-- navigation bar at the top -->
+<header>
+    <!-- Just an image -->
+    <nav class="navbar navbar-light bg-faded bg-info text-white">
+        <a class="navbar-brand"  id="logoSite" href="">
+            <img class="col-lg-12" id="logo" src="/images/logo.png" alt="Cat Wishes logo">
+
+        </a>
+
+        <ul class="list-inline">
+            <li>
+            <a href="" class="text-danger list-inline-item"> <h4>Toys</h4> </a>
+
+
+
+            </li>
+        </ul>
+
+    </nav>
+</header>
+
+<!-- JQuery for user Login supplies correct path for both parameterized and unparameterized pages -->
+<?php
+
+    echo "<p>included standard header</p>";
+}
+
+function standard_footer( ){
+    echo "<p>included standard footer</p>";
+    ?>
+    <!-- bootstrap jquery/popper/javascript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"
+            integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4"
+            crossorigin="anonymous"></script>
+
+
+    </body>
+    </html>
+
+    <?php
+}
+    ?>
