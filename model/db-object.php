e7155b34647b4fff8583d3eb91bd74b54def51fe
Jhakon Pappoe <j.pappoe@hotmail.com>
2018-04-13 14:27:33 -0400

    removed connect assignment

--- model/db-object.php
+++ model/db-object.php
@@ -8,32 +8,32 @@ require '/home/jpappoeg/config.php';
  * @version 1.0
  * @copyright  2018
  */
-
-/**
- * Function to connect to db
- * @return PDO database connection
- */
-function connect()
+class DBObject
 {
-    try {
-        //Instantiate a database object
-        $dbh = new PDO(DB_DSN, DB_USERNAME,
-            DB_PASSWORD);
-        echo "Connected to database!!!";
-        return $dbh;
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-        return;
+    /**
+     * Function to connect to db
+     * @return PDO database connection
+     */
+    function connect()
+    {
+        try {
+            //Instantiate a database object
+            $dbh = new PDO(DB_DSN, DB_USERNAME,
+                DB_PASSWORD);
+            echo "Connected to database!!!";
+            return $dbh;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return;
+        }
     }
-}
 
-$dbh = connect();
-
-/**
- * Function to disconnect from the database
- */
-function disconnect()
-{
-    global $dbh;
-    $dbh = "";
+    /**
+     * Function to disconnect from the database
+     */
+    function disconnect()
+    {
+        global $dbh;
+        $dbh = "";
+    }
 }
