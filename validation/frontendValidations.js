104bc115c7029bcd135fa47602f14cd8f40a230f
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-04-18 16:58:50 -0400

    corrected errors in front end validation

--- validation/frontendValidations.js
+++ validation/frontendValidations.js
@@ -1,74 +1,85 @@
-/*
-  Need to comment this and continue creating validations.
-  Need to figure out how to test what I have.
- */
-
-function depositbyValidation(depositBy){
-    var depositBy = document.forms["transactionForm"]["depositby"].value;
-
-    if(!emptyStringPrevalidation(depositBy)){
-        return "Deposit By is required";
-    }
-    if(!alphStringPrevalidation(depositBy) ){
-        return "Name contained nonAlphabetical call"
-    }
-}
-
-function dateValidation(date){
-    var date = document.forms["transactionForm"]["date"].value;
-}
-
-function categoryValidation(category){
-    if(!emptyStringPrevalidation(category)){
-        return "Deposit By is required";
-    }
-    if(!alphStringPrevalidation(category) ){
-        return "Name contained nonAlphabetical call"
-    }
-}
-
-function quantityValidation(quantity){
-    return false;
-}
-
-function descriptionValidation(description){
-    return false;
-}
-
-function unitPriceValidation(unitPrice){
-    return priceValidation(unitPrice);
-}
-
-function totalPriceValidation(totalPrice){
-    return priceValidation(totalPrice);
-}
-
-function amountPaidOrDiscountValidation(amount){
-    return priceValidation(amount);
-}
-
-function paymentTypeValidation(type){
-    var types = ["Cash", "Check", "Credit Card"];
-    if(types.indexOf("type") >= 0){
-        return true;
-    };
-    return false
-}
-
+// /*
+//   Need to comment this and continue creating validations.
+//   Need to figure out how to test what I have.
+//  */
+//
+// function depositbyValidation(depositBy){
+//     var depositBy = document.forms["transactionForm"]["depositby"].value;
+//
+//     if(!emptyStringPrevalidation(depositBy)){
+//         return "Deposit By is required";
+//     }
+//     if(!alphStringPrevalidation(depositBy) ){
+//         return "Name contained nonAlphabetical call"
+//     }
+// }
+//
+// function dateValidation(date){
+//     var date = document.forms["transactionForm"]["date"].value;
+// }
+//
+// function categoryValidation(category){
+//     if(!emptyStringPrevalidation(category)){
+//         return "Deposit By is required";
+//     }
+//     if(!alphStringPrevalidation(category) ){
+//         return "Name contained nonAlphabetical call";
+//     }
+// }
+//
+// function quantityValidation(quantity){
+//     return false;
+// }
+//
+// function descriptionValidation(description){
+//     return false;
+// }
+//
+// function unitPriceValidation(unitPrice){
+//     return priceValidation(unitPrice);
+// }
+//
+// function totalPriceValidation(totalPrice){
+//     return priceValidation(totalPrice);
+// }
+//
+// function amountPaidOrDiscountValidation(amount){
+//     return priceValidation(amount);
+// }
+//
+// function paymentTypeValidation(type){
+//     var types = ["Cash", "Check", "Credit Card"];
+//     if(types.indexOf("type") >= 0){
+//         return true;
+//     };
+//     return false
+// }
+//
 function priceValidation(price){
-    return false;
-}
 
-function emptyStringPrevalidation(anyString){
-    if (anyString == "") {
+        if(price.match(/^\d+\.?\d{0,2}$/) ){
+            return true;
+        }
         return false;
-    }
-    return true;
-}
 
+}
+//
+//
+//
+// function emptyStringPrevalidation(anyString){
+//     if (anyString == "") {
+//         return false;
+//     }
+//     return true;
+// }
+//
 function alphStringPrevalidation(anyString){
-    if(anyString.match("^[a-zA-Z\(\)]+$") ){
+    if(anyString.match(/^[a-zA-Z]+$/) ){
         return true;
     }
     return false;
+}
+
+function sayHelloscript(){
+    alert("Hello");
 }
