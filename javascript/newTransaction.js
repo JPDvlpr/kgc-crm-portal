08121040931f789ee9d882c6f511d3a0eb65b5b3
Tyler Roemer <troemer@mail.greenriver.edu>
2018-04-18 16:08:42 -0400

    new Transaction form now displays subcategories from database when category is selected using ajax

--- javascript/newTransaction.js
+++ javascript/newTransaction.js
@@ -1,20 +1,81 @@
-$(document).on('click', 'input.category', function (){
+var optionsString = ""
+for(var i = 1; i <= 10; i++)
+    optionsString += "<option>"+i+"</option>";
+
+//add drop down to categories(reasons for deposit)
+$(document).on('click', 'label.category', function (){
+    var category = $(this).attr('id');
     $.ajax({
         type: "POST",
-        url: "something.php",
-        data: { category: $(this).val() },
+        url: "http://troemer.greenriverdev.com/355/kgc-crm-portal-team/views/skus_temp.php",
+        dataType:"json",
+        data: { category: category },
         success: function (data) {
-            var stuff = data;
+            $('.subCategory').remove();
+            if(category == 'class' || category == 'event'){
+                for(var i = 0; i < data.length; i++) {
+                    $('#' + category).after(
+                        "<div class='radio subCategory'>" +
+                            "<label><input type='radio' name='class' " +
+                                "value='" + data[i]['sku_id'] + "'> " +
+                                data[i]['name'] + " - $" + data[i]['price'] +
+                            "  </label>" +
+                            "<select name='quantity'>" +
+                                optionsString +
+                            "</select>" +
+                        "</div>");
+                }
+            }
+            else if(category == 'donation'){
+                $('#'+category).after(
+                    "<div class='radio subCategory'>"+
+                        "<label class=''><input id='cash' type='radio' name='donation' "+
+                        "value='"+data[1]['sku_id']+"'> "+data[1]['name']+"  </label>"+
+                        "<div class='input-group'>"+
+                            "<div class='input-group-prepend'>"+
+                                "<span class='input-group-text'>$</span>"+
+                            "</div>"+
+                            "<input id='cashDonation' type='text' class='form-control'>"+
+                        "</div>"+
+                    "</div>");
+                $('#'+category).after(
+                    "<div class='radio subCategory'>"+
+                        "<label><input id='item' type='radio' name='donation' "+
+                        "value='"+data[0]['sku_id']+"'> "+data[0]['name']+"  </label>"+
+                        "<select name='quantity'>"+
+                            optionsString+
+                        "</select>"+
+                        "<input id='itemDonation' type='text' class='form-control' placeholder='Donated Item'> "+
+                    "</div>");
+            };
         },
-        dataType:"json"
+        error: function(xhr, textStatus, thrownError, data) {
+            alert("Error: " + thrownError);
+        }
     });
 });
 
-$(document).on('click', 'input.payMethod', function () {
-    if($(this).val() == "check"){
+//when amount/description is typed in for item or cash donation it will
+//automatically click radio button(if it isn't already).
+$(document).on('input', '#itemDonation, #cashDonation', function () {
+    var radioButton = $(this).attr('id').substring(0,4);
+    $("#"+radioButton).prop("checked", true);
+});
 
+//add drop downs for check & credit card payment methods
+$(document).on('click', 'input.payMethod', function () {
+    $('.hidden').css("display", "none");
+
+    if($(this).val() == "check"){
+        $('#checkNum').css("display", "block");
     }
     else if($(this).val() == "credit"){
-
+        $('#paypal').css("display", "block");
+        $('#square').css("display", "block");
     }
+})
+
+//display add button after subcategory is selected
+$(document).on('click', '.subcategory', function () {
+
 });
