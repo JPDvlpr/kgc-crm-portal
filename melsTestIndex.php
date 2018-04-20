9fc07c55093c3762fe7937847063faa7e990a230
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-04-20 13:47:54 -0400

    validation of js and php

---
+++ melsTestIndex.php
@@ -0,0 +1,52 @@
+<?php
+require_once("views/standard_peices.php");
+require_once("validation/backendValidations.php");
+standard_header("sample", "");
+
+echo "hello 3";
+$errors;
+require_once("views/categories.php");
+if (validateCategory('Class', $errors)) {
+    echo "Class was valid";
+}
+
+//    require_once("views/skus.php");
+?>
+    <script src="validation/frontendValidations.js"></script>
+    <div id="results">Nothing</div>
+    <script>
+
+        if (isAlphString('')) {
+            $('#results').append('accepted empty string<br>');
+        } else {
+            $('#results').append('rejected empty string<br>');
+
+        }
+        if (isAlphString('category234')) {
+            $('#results').append('category234<br>');
+        }
+        if (isAlphString('category')) {
+            $('#results').append('category<br>');
+        }
+        if (isPrice('4.25')) {
+            $('#results').append('$4.15<br>');
+        }
+        if (isPrice('4.2')) {
+            $('#results').append('$4.2<br>');
+        }
+        if (isPrice('54444444.2')) {
+            $('#results').append('$544444.2<br>');
+        }
+        if (isPrice('4.')) {
+            $('#results').append('$4.<br>');
+        }
+        if (isPrice('4')) {
+            $('#results').append('$4done<br>');
+        }
+        if (isPrice('four')) {
+            $('#results').append('four<br>');
+        }
+    </script>
+<?php
+standard_footer();
+?>
---
+++ validation/clientSideValidations.js
@@ -0,0 +1,8 @@
+$('#itemDonation').change(function() {
+    var temp = $('#itemDonation').val()
+        if(alph$('#login').hide("slow");
+        $('.member').append(user);
+        $('.member').append(pass);
+        $('.member').append(submit);
+    }
+);
