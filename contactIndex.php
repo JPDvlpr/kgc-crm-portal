3d9fc7ce412abf17d7e915accb3bfd1caf6220be
Kevan Barter <Kbarter@mail.greenriver.edu>
2018-05-09 17:06:19 -0400

    modfications to the contact form, contact Index page, and backend validation from last class that will help with validation

---
+++ contactIndex.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("views/standard_peices.php");
+
+    standard_header("New Contact","./styles/contact.css");
+
+    require_once("views/newContact.php");
+
+    standard_footer();
+?>
--- views/newContact.php
+++ views/newContact.php
@@ -14,6 +14,7 @@
     <title>New Contact</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../styles/contact.css">
 </head>
 
 <body>
@@ -30,31 +31,39 @@
                     </tr>
                     <tr>
                         <td colspan="2">Address:</td>
-                        <td colspan="4"><input id="contactAddress" type="text" name="address" class="form-control"></td>
-                    </tr>
-                    <tr>
-                        <td colspan="1">City:</td>
-                        <td colspan="1"><input id="contactCity" type="text" name="city" class="form-control"></td>
-                        <td colspan="1">State:</td>
-                        <td colspan="1">
-                            <select name="state">
-                                <?php
+                        <td colspan="4">
+                            <input id="contactAddress" type="text" name="address" class="form-control" placeholder="Street Address">
+                            <div class="input-group w-100">
+                                <input id="contactCity" type="text" name="city" class="form-control my-2" placeholder="City">
+                                <select id="states" name="state" class="form-control m-2">
+                                    <?php
                                     foreach ($states as $state){
                                         echo '<option value="'.$state.'">'.$state.'</option>';
                                     }
-                                ?>
-                            </select>
+                                    ?>
+                                </select>
+                                <input id="contactZipCode" type="text" name="city" class="form-control my-2" placeholder="Zip Code">
+                            </div>
+
                         </td>
-                        <td colspan="1">Zip:</td>
-                        <td colspan="1"><input id="contactZipCode" type="text" name="city" class="form-control"></td>
                     </tr>
                     <tr>
-                        <td colspan="2">Phone #:<input id="contactPhone" type="text" name="phone" class="form-control"></td>
-                        <td colspan="2">Cell Phone #:<input id="contactCellPhone" type="text" name="cellPhone" class="form-control"></td>
-                        <td colspan="2">Email Address:<input id="contactEmail" type="email" name="email" class="form-control"></td>
+                        <td colspan="2">Phone:</td>
+                        <td colspan="4">
+                            <div id="cellPhone" class="input-group w-100">
+                                (cell)<input id="contactCellPhone" type="text" name="cellPhone" class="form-control m-2">
+                            </div>
+                            <div id="otherPhone" class="input-group w-100">
+                                (other)<input id="contactPhone" type="text" name="phone" class="form-control m-2">
+                            </div>
+                        </td>
                     </tr>
                     <tr>
-                        <td colspan="2">Preferred Name:</td>
+                        <td colspan="2">Email Address:</td>
+                        <td colspan="4"><input id="contactEmail" type="email" name="email" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Alternate Name:</td>
                         <td colspan="4"><input id="contactAltName" type="text" name="altName" class="form-control"></td>
                     </tr>
                     <tr>
