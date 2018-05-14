b74d42fb8f58dee357377730b500fa575a872d34
Jhakon Pappoe <j.pappoe@hotmail.com>
2018-05-14 16:25:51 -0400

    updated header

--- jhakonTest.php
+++ jhakonTest.php
@@ -1,3 +1,11 @@
 <?php
-echo "hello";
 require_once("validation/backendValidations.php");
+validateDate('04/11/2015');
+echo'<br>';
+validateInteger(1);
+echo'<br>';
+validateEmail('a@m.com');
+echo'<br>';
+validateAddress('1234 main st se se 4');
+echo'<br>';
+validateName('a');
