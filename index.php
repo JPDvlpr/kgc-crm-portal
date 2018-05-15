015e5c011e695fd327ad48f97469d1f458d94930
Tyler Roemer <troemer@mail.greenriver.edu>
2018-05-15 13:55:08 -0400

    Added some absolute paths

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     require_once("views/standard_peices.php");
 
 
-    standard_header("New Transaction","./styles/temp.css");
+    standard_header("New Transaction","/355/kgc-crm-portal-team/styles/temp.css");
 
     require_once("views/newTransaction.php");
 
-    standard_footer("./javascript/transactionEvents.js");
+    standard_footer("/355/kgc-crm-portal-team/javascript/transactionEvents.js");
 ?>
