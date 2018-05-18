8bdab7d7504757212b93f84d26331d296ba9a569
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-05-17 21:16:49 -0400

    GenerateCSV will not update the file names and delete older copies. Now to get some real data.

--- controller/generateCSV.php
+++ controller/generateCSV.php
@@ -1,46 +1,60 @@
 <?php
 /*
- *  details.php
- *  Cat-Wishes Final Project
- *  IT-328
- *  Melanie Felton
+ *  generateCSV.php
+ *  KGC CRM Portal
+ *  IT-55
+ *  Just oK TeaM
  *
- *  This file is called by the AJAX and retrieves a single item, returns with the HTML formatting
+ *  This file is called by the AJAX, creates a csv file, and returns with that files name
+ *  It loops through a series of 6 filenames so that the multiple u
  */
-ini_set("display_errors", 1);
-error_reporting(E_ALL);
-include_once("../model/db-object.php");
-include_once("../model/db-item.php");
-include_once("../model/db-categories.php");
-include_once("../model/transaction.php");
 
-$getItems = function ($table, $category) {
+$getCSVFile = function ($table, $category) {
 //    $transactions = Transaction::getTransactions();
     $transactions = array(array('Mel23', '24', '99'), array('Tye', '67', '25'), array('Kev', '45', '2'), array('Joe', '33', '44'));
+    $fileName = "LATEST_FILE";
+    $count = 100;
+    $fileNameTemp = $fileName.$count.".csv";
 
+    //increment file number and delete oldest...
+    // currently only allowing at most 6 files to exist
 
-    $file = fopen('../files/LATEST_FILE.csv', 'wb');
+    //find an unused name
+    while (file_exists('../files/'.$fileNameTemp)) {
+        $count++;
+        $fileNameTemp = $fileName.$count.".csv";
+    }
+    //look to see if there is one 5 iterations older
+    if (file_exists('../files/'.$fileName . ($count - 5).".csv")) {
+        unlink('../files/'.$fileName . ($count - 5).".csv");
+    }
+    //May need to be removed because it may cause trouble for two users - should test with
+    //two users
+    //or if we have looped around, then the next one
+    if (file_exists('../files/'.$fileName . ($count + 1).".csv")) {
+        unlink('../files/'.$fileName . ($count + 1).".csv");
+    }
+    //assign new name to $fileName
+    $fileName = $fileNameTemp;
+
+    //open file
+    $file = fopen('../files/'.$fileName, 'wb');
+    //store column headers
     $store = array("dont Have", "transaction");
     fputcsv($file, $store);
+
+    //store column values
     foreach ($transactions as $transaction) {
         $store = array("Have", "transaction");
         fputcsv($file, $store);
 //        fputcsv($file, $transaction);
     }
-    fclose($file);
-//    while (!file_exists('../files/LATEST_FILE.csv')) {
-//        for ($i = 0; $i < 10; $i++) {
-//
-//        }
-//    }
 
-    $filename = array();
-    array_push($filename, array(
-        'filename' => 'latestFile.csv'));
-    echo $filename;
-//    echo json_encode($file);
+    // close file and return correct filename
+    fclose($file);
+    echo $fileName;
 
 };
-$getItems("item", $_POST['category']);
+$getCSVFile("item", $_POST['category']);
 
 ?>
--- javascript/generateCSV.js
+++ javascript/generateCSV.js
@@ -1,25 +1,17 @@
 //creates csv file
 $(document).on('click', '#generate', function (){
     $('#generate').html('Generating');
-    // $('#generate').replaceWith('<button id="generate"><a href="filename">Download CSV</a></button>');
 
     var filename = $(this).attr('href');
     var me = $(this);
     $.ajax({
         type: "POST",
-        // url: "views/items_temp.php",
         url: "controller/generateCSV.php",
 
-        // dataType:"json",
-        // data: { category: category },
-        // data: { filename: filename},
-        // success: function (data) {
         success: function (results) {
-                // $('.subCategory').remove();
-            // $('#add').remove();
-            alert("Still need to dynamically get filename to download");
-            alert("Also it seems that I get into success before the file has actually written.");
-            $('#generate').replaceWith('<button id="download"><a href="files/LATEST_FILE.csv" download>Download CSV</a></button>');
+            var filename = "files/" + results;
+            $('#generate').replaceWith('<button id="download"><a href="' + filename+ '"download>Download CSV</a></button>');
+            console.log(filename);
         },
         error: function(xhr, textStatus, thrownError, data) {
             alert("Error: " + thrownError);
