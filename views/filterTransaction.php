73198476b9c77b8aa520f4d68f547d19849f1bca
Jhakon Pappoe <j.pappoe@hotmail.com>
2018-05-22 17:27:22 -0400

    filter form set up and refactored

--- views/filterTransaction.php
+++ views/filterTransaction.php
@@ -8,60 +8,64 @@ error_reporting(E_ALL);
     <!-- CDN minimized bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
           integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/filters.css">
+    <!--    <link rel="stylesheet" href="../styles/filters.css">-->
 
 </head>
 <body>
 
 <form action="#" method="post">
     <div class="container">
-        <h1 class="display-4">Filter Transactions</h1>
-        <div class="row">
-            <div class="col-md-1 form-group">
-                Date: <input type="checkbox"><br>
-            </div>
-            <div class="col-md-3 form-group">
-                <h4>Start Date:</h4>
-                <input type="date" class="form-control" id="from_date" name="from_date"
-                       value="<?php echo date("Y-01-01") ?>">
-            </div>
-            <div class="col-md-3 form-group">
-                <h4>End Date:</h4>
-                <input type="date" class="form-control" id="to_date" name="to_date"
-                       value="<?php echo date("Y-m-d") ?>">
-            </div>
-            <div class="col-md-3 ">
-                <input type="submit" class="btn btn-secondary" name="submit" id="filter" value="Filter">
-            </div>
-        </div>
+        <table class="table table-bordered">
+            <thead>
+            <th colspan="6"><strong>Filter Transactions</strong></th>
+            </thead>
+            <tbody>
+            <tr>
+                <th>Start Date:</th>
+                <th>End Date:</th>
+            </tr>
+            <tr>
+                <td>
+                    <input type="date" class="form-control" id="from_date" name="from_date"
+                           value="<?php echo date("Y-01-01") ?>">
+                </td>
+                <td>
+                    <input type="date" class="form-control" id="to_date" name="to_date"
+                           value="<?php echo date("Y-m-d") ?>">
+                </td>
+            </tr>
+            <tr>
+                <th>Contact Columns</th>
+                <th>Contacts</th>
+            </tr>
+            <tr>
+                <td>
+                    <input id="chosenContact" type=text list=contacts class="form-control">
+                    <datalist id="contacts">
+                        <select id="columnselect">
+                            <?php
+                            $columns = array("contact_name", "city", "state", "zip");
+                            foreach ($columns as $col)
+                                echo "<option id = '$col' value='$col'>" . $col . "</option>";
+                            ?>
+                        </select>
+                    </datalist>
+                </td>
+                <td>
+                    <input id="chosenContact" type=text list=contacts class="form-control">
+                    <datalist id="contacts">
+                        <select id="columnselect">
+                            <option>
+                                Bob Smith
+                            </option>
+                        </select>
+                    </datalist>
+                </td>
+            </tr>
+            </tbody>
+        </table>
     </div>
-</form>
-
-<form action="#" method="post">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 form-group">
-                <h4>Contacts</h4>
-                <input id="chosenContact" type=text list=contacts class="form-control">
-                <datalist id="contacts">
-                    <select id="columnselect">
-                        <?php
-                        $columns = array("contact_name", "city", "state", "zip");
-                        foreach ($columns as $col)
-                            echo "<option id = '$col' value='$col'>" . $col . "</option>";
-                        ?>
-                    </select>
-                </datalist>
-            </div>
-
-            <div class="col-md-3 form-group">
-                <h4>Contacts</h4>
-                <input id="chosenContact" type=text list=contacts class="form-control">
-                <datalist id="contacts">
-                </datalist>
-            </div>
-        </div>
-    </div>
+    <input type="submit" class="btn btn-secondary" name="submit" id="filter" value="Filter">
 </form>
 <?php
 //require_once("../model/db-filters.php");
