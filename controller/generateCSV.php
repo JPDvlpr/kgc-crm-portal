be1cc7eed9b21d2998f8f94b869dd7e426ea8970
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-05-23 15:49:52 -0400

    Redoing stuff that git lost

--- controller/generateCSV.php
+++ controller/generateCSV.php
@@ -6,13 +6,17 @@
  *  Just oK TeaM
  *
  *  This file is called by the AJAX, creates a csv file, and returns with that files name
- *  It loops through a series of 6 filenames so that the multiple u
+ *  It loops through a series of 6 filenames so that the multiple users can create files at
+ *  once.
  */
 
 $getCSVFile = function ($table, $category) {
-//    $transactions = Transaction::getTransactions();
-    $transactions = array(array('Mel23', '24', '99'), array('Tye', '67', '25'), array('Kev', '45', '2'), array('Joe', '33', '44'));
-    $fileName = "LATEST_FILE";
+    $fileName = $_POST['filteringData']['filename'];
+    $filters = $_POST['filteringData']['filters'][0];
+//    var_dump($filters);
+    $transactions = Transaction::getTransactions($filters);
+//    $transactions = array(array('Mel23', '24', '99'), array('Tye', '67', '25'), array('Kev', '45', '2'), array('Joe', '33', '44'));
+//    $fileName = "LATEST_FILE";
     $count = 100;
     $fileNameTemp = $fileName.$count.".csv";
 
@@ -25,15 +29,16 @@ $getCSVFile = function ($table, $category) {
         $fileNameTemp = $fileName.$count.".csv";
     }
     //look to see if there is one 5 iterations older
-    if (file_exists('../files/'.$fileName . ($count - 5).".csv")) {
-        unlink('../files/'.$fileName . ($count - 5).".csv");
-    }
     //May need to be removed because it may cause trouble for two users - should test with
     //two users
     //or if we have looped around, then the next one
-    if (file_exists('../files/'.$fileName . ($count + 1).".csv")) {
+    if (file_exists('../files/'.$fileName . ($count - 5).".csv")) {
+        unlink('../files/'.$fileName . ($count - 5).".csv");
+    }
+    elseif (file_exists('../files/'.$fileName . ($count + 1).".csv")) {
         unlink('../files/'.$fileName . ($count + 1).".csv");
     }
+
     //assign new name to $fileName
     $fileName = $fileNameTemp;
 
@@ -45,7 +50,7 @@ $getCSVFile = function ($table, $category) {
 
     //store column values
     foreach ($transactions as $transaction) {
-        $store = array("Have", "transaction");
+        $store = array($transaction['trans_id'],$transaction['trans_name']);
         fputcsv($file, $store);
 //        fputcsv($file, $transaction);
     }
--- melsTestIndex.php
+++ melsTestIndex.php
@@ -10,52 +10,52 @@ echo "hello 3";
 include("views/generateCSVButton.php");
 //$errors;
 ?>
-<!---->
-<!--<php?-->
-<!--//require_once("views/categories.php");-->
-<!--//if (validateCategory('Class', $errors)) {-->
-<!--//    echo "Class was valid";-->
-<!--//}-->
-<!--//echo "hi";-->
-<!---->
-<!--require_once("views/items.php");-->
-<!---->
-<!--?>-->
-<!--    <script src="validation/frontendValidations.js"></script>-->
-<!--    <div id="results">Nothing2</div>-->
-<!--<!--    <script>-->-->
-<!---->
-<!--        if (isAlphString('')) {-->
-<!--            $('#results').append('accepted empty string<br>');-->
-<!--        } else {-->
-<!--            $('#results').append('rejected empty string<br>');-->
-<!---->
-<!--        }-->
-<!--        if (isAlphString('category234')) {-->
-<!--            $('#results').append('category234<br>');-->
-<!--        }-->
-<!--        if (isAlphString('category')) {-->
-<!--            $('#results').append('category<br>');-->
-<!--        }-->
-<!--        if (isPrice('4.25')) {-->
-<!--            $('#results').append('$4.15<br>');-->
-<!--        }-->
-<!--        if (isPrice('4.2')) {-->
-<!--            $('#results').append('$4.2<br>');-->
-<!--        }-->
-<!--        if (isPrice('54444444.2')) {-->
-<!--            $('#results').append('$544444.2<br>');-->
-<!--        }-->
-<!--        if (isPrice('4.')) {-->
-<!--            $('#results').append('$4.<br>');-->
-<!--        }-->
-<!--        if (isPrice('4')) {-->
-<!--            $('#results').append('$4done<br>');-->
-<!--        }-->
-<!--        if (isPrice('four')) {-->
-<!--            $('#results').append('four<br>');-->
-<!--        }-->
-<!--    </script>-->
+    <!---->
+    <!--<php?-->
+    <!--//require_once("views/categories.php");-->
+    <!--//if (validateCategory('Class', $errors)) {-->
+    <!--//    echo "Class was valid";-->
+    <!--//}-->
+    <!--//echo "hi";-->
+    <!---->
+    <!--require_once("views/items.php");-->
+    <!---->
+    <!--?>-->
+    <!--    <script src="validation/frontendValidations.js"></script>-->
+    <!--    <div id="results">Nothing2</div>-->
+    <!--<!--    <script>-->-->
+    <!---->
+    <!--        if (isAlphString('')) {-->
+    <!--            $('#results').append('accepted empty string<br>');-->
+    <!--        } else {-->
+    <!--            $('#results').append('rejected empty string<br>');-->
+    <!---->
+    <!--        }-->
+    <!--        if (isAlphString('category234')) {-->
+    <!--            $('#results').append('category234<br>');-->
+    <!--        }-->
+    <!--        if (isAlphString('category')) {-->
+    <!--            $('#results').append('category<br>');-->
+    <!--        }-->
+    <!--        if (isPrice('4.25')) {-->
+    <!--            $('#results').append('$4.15<br>');-->
+    <!--        }-->
+    <!--        if (isPrice('4.2')) {-->
+    <!--            $('#results').append('$4.2<br>');-->
+    <!--        }-->
+    <!--        if (isPrice('54444444.2')) {-->
+    <!--            $('#results').append('$544444.2<br>');-->
+    <!--        }-->
+    <!--        if (isPrice('4.')) {-->
+    <!--            $('#results').append('$4.<br>');-->
+    <!--        }-->
+    <!--        if (isPrice('4')) {-->
+    <!--            $('#results').append('$4done<br>');-->
+    <!--        }-->
+    <!--        if (isPrice('four')) {-->
+    <!--            $('#results').append('four<br>');-->
+    <!--        }-->
+    <!--    </script>-->
 <?php
 //    $transDate = date("Y-m-d H:i:s");
 ////$itemDesc = $item[3];
