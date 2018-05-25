81053ef5e36e2f3c5226e66d45bb585f909fc363
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-05-25 14:27:13 -0400

    Working on preview

--- controller/previewDataCSV.php
+++ controller/previewDataCSV.php
@@ -6,6 +6,7 @@
  *  Just oK TeaM
  *
  *  This file is called by the AJAX, and retrieves the filtered results for previewing
+ *  Returns just the contact name, item description, total amount, and trans date
  */
 include_once($_SERVER['DOCUMENT_ROOT']."/kgc-crm-portal-team/model/db-transaction.php");
 
@@ -15,8 +16,16 @@ $previewCSVData = function () {
     unset($filters['filename']);
 
     $transactions = DBTransaction::getFilteredTransactions($filters);
+    $previews = array();
 
-    return $transactions;
+    foreach ($transactions as $transaction) {
+        $store = array($transaction['trans_date'],$transaction['contact_name'],
+            $transaction['total_amount'],$transaction['item_desc']);
+        array_push($previews, $store);
+//        fputcsv($file, $transaction);
+    }
+
+    return $previews;
 
 };
 $previewCSVData();
