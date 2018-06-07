3fc56d8d2a40f43533ee02c0149acf2ac35ea7ea
Jhakon Pappoe <j.pappoe@hotmail.com>
2018-06-07 16:35:56 -0400

    return admins in contact form created by

--- views/getAdmins.php
+++ views/getAdmins.php
@@ -1,8 +1,11 @@
 <?php
 
-    include_once($_SERVER['DOCUMENT_ROOT']."/kgc-crm-portal-team/model/db-admins.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/kgc-crm-portal-team/model/db-admins.php");
 
-    $results = DBAdmin::getAdmins();
+$results = DBAdmin::getAdmins();
 
-    echo json_encode($results);
+// added for each to retrieve admins
+foreach ($results as $result) {
+    echo "<option value='" . $result['admin_id'] . "'>" . $result['admin_name'] . '</option>';
+}
 ?>
