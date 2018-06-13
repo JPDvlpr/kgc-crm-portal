4fc0c3f7298ade62cce5991d5ec624a87356be48
Jhakon Pappoe <j.pappoe@hotmail.com>
2018-06-13 16:07:17 -0400

    documentation in db-admins.php

--- model/db-admins.php
+++ model/db-admins.php
@@ -1,23 +1,29 @@
 <?php
 require_once('db-object.php');
 
+/**
+ * Class DBAdmin
+ */
 class DBAdmin extends DBObject
 {
+    /**
+     * @param admins table
+     * @return results
+     * gets everything from the admins table
+     */
     public static function getAdmins($table = 'admins')
     {
+//        database object
         global $dbh;
         $dbh = (new self)->connect();
-
-
+//        retrieve everything from admins table
         $sql = "SELECT * FROM " . $table;
+//        prepare sql statement
         $statement = $dbh->prepare($sql);
         $statement->execute();
+//        retrieve result row as an associative array
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
         (new self)->disconnect();
         return $results;
     }
 }
-
-
